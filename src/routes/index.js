41a86988efe983ee4f3302cd3b606474cd51f9dd
vishnuprabhakar2001 <115687030+vishnuprabhakar2001@users.noreply.github.com>
2026-02-03 00:27:28 +0530

    initial test

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/health", (req, res) => {
+  // just for initial test
   res.status(200).json({
     success: true,
     message: "Server is running",
